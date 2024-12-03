fd876997f70bce5d65972e4f3149845fc6945d89
Taner <tanerdokmets@gmail.com>
2024-12-03 21:03:48 +0300

    tt-yazilim
    project finish

---
+++ src/constants/index.js
@@ -0,0 +1,288 @@
+import {
+  benefitIcon1,
+  benefitIcon2,
+  benefitIcon3,
+  benefitIcon4,
+  benefitImage2,
+  chromecast,
+  disc02,
+  discord,
+  discordBlack,
+  facebook,
+  figma,
+  file02,
+  framer,
+  homeSmile,
+  instagram,
+  notification2,
+  notification3,
+  notification4,
+  notion,
+  photoshop,
+  plusSquare,
+  protopie,
+  raindrop,
+  recording01,
+  recording03,
+  roadmap1,
+  roadmap2,
+  roadmap3,
+  roadmap4,
+  searchMd,
+  slack,
+  sliders04,
+  telegram,
+  twitter,
+  yourlogo,
+} from "../assets";
+
+export const navigation = [
+  {
+    id: "0",
+    title: "HİZMETLERİMİZ",
+    url: "#hizmetlerimiz",
+  },
+  {
+    id: "1",
+    title: "NEDEN BİZ?",
+    url: "#neden-biz",
+  },
+  {
+    id: "2",
+    title: "İLETİŞİM",
+    url: "#iletisim",
+  },
+];
+
+export const heroIcons = [homeSmile, file02, searchMd, plusSquare];
+
+export const notificationImages = [notification4, notification3, notification2];
+
+export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+
+export const brainwaveServices = [
+  "Kullanıcı Dostu Çözümler",
+  "Hızlı ve Etkili İletişim",
+  "SEO ve Performans Odaklı",
+  "Sürekli Destek ve Bakım"
+];
+
+
+export const brainwaveServicesIcons = [
+  recording03,
+  recording01,
+  disc02,
+  chromecast,
+  sliders04,
+];
+
+export const roadmap = [
+  {
+    id: "0",
+    title: "Kurumsal Kimlik Tasarımı",
+    text: "Tek bakışta tanınan bir marka olmanızı sağlayacak logo, kurumsal evrak, kartvizit, katalog v.b çalışmalarınızı yapmak için sabırsızlanıyoruz.",
+    date: "TT YAZILIM",
+    status: "done",
+    imageUrl: roadmap1,
+    colorful: true,
+  },
+  {
+    id: "1",
+    title: "Dijital Tasarımlar",
+    text: "Markalar için özelleştirilmiş, yaratıcı web tasarımları sunarak dijital dünyada güçlü bir iz bırakmalarını sağlıyoruz. Hedef odaklı, estetik ve kullanıcı dostu çözümlerle çevrimiçi başarıyı arttırıyoruz.",
+    date: "TT YAZILIM",
+    status: "done",
+    imageUrl: roadmap2,
+  },
+  {
+    id: "2",
+    title: "Dijital Çözümler",
+    text: "Dijital dünyada rekabet avantajı sağlamak için stratejik çözümler, yenilikçi ürünler ve kullanıcı odaklı hizmetler geliştiriyoruz.",
+    date: "TT YAZILIM",
+    status: "done",
+    imageUrl: roadmap3,
+  },
+  {
+    id: "3",
+    title: "Analiz",
+    text: "Ürün veya servisiniz için belirlediğiniz hedefleri kavrayarak, süreç analizi ve iş geliştirme danışmanlığı yapıyoruz.",
+    date: "TT YAZILIM",
+    status: "done",
+    imageUrl: roadmap4,
+  },
+];
+
+export const collabText =
+  "Mükemmel bir web tasarımı sadece görsel açıdan etkileyici değil, aynı zamanda kullanıcı dostu ve işlevsel olmalıdır. Biz, her projeye özel, estetik açıdan zarif ve kullanıcı deneyimini ön planda tutarak web siteleri tasarlıyoruz.";
+
+
+  export const collabContent = [
+    {
+      id: "0",
+      title: "Estetik ve Kullanıcı Dostu Tasarımlar",
+      text: collabText,
+    },
+    {
+      id: "1",
+      title: "Modern ve Minimalist Yaklaşım",
+    },
+    {
+      id: "2",
+      title: "Hız ve Estetik Uyumu",
+    },
+  ];
+  
+export const collabApps = [
+  {
+    id: "0",
+    title: "Figma",
+    icon: figma,
+    width: 26,
+    height: 36,
+  },
+  {
+    id: "1",
+    title: "Notion",
+    icon: notion,
+    width: 34,
+    height: 36,
+  },
+  {
+    id: "2",
+    title: "Discord",
+    icon: discord,
+    width: 36,
+    height: 28,
+  },
+  {
+    id: "3",
+    title: "Slack",
+    icon: slack,
+    width: 34,
+    height: 35,
+  },
+  {
+    id: "4",
+    title: "Photoshop",
+    icon: photoshop,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "5",
+    title: "Protopie",
+    icon: protopie,
+    width: 34,
+    height: 34,
+  },
+  {
+    id: "6",
+    title: "Framer",
+    icon: framer,
+    width: 26,
+    height: 34,
+  },
+  {
+    id: "7",
+    title: "Raindrop",
+    icon: raindrop,
+    width: 38,
+    height: 32,
+  },
+];
+
+export const pricing = [
+  {
+    id: "0",
+    title: "Modern Tasarım",
+    description: "Kullanıcı dostu, modern ve şık web tasarımları ile markanızı dijital dünyada ön plana çıkarıyoruz.",
+    features: [
+      "Daha Hızlı",
+      "Daha Estetik",
+      "Daha İşlevsel",
+    ],
+  },
+  {
+    id: "1",
+    title: "Hızlı ve Güvenilir",
+    description: "Web sitenizin hızını ve güvenliğini ön planda tutarak, kesintisiz bir kullanıcı deneyimi sunuyoruz.",
+    features: [
+      "Daha Güçlü",
+      "Daha Hızlı",
+      "Daha Kesintisiz",
+    ],
+  },
+  {
+    id: "2",
+    title: "SEO Optimizasyonu",
+    description: "SEO uyumlu web sayfaları ile arama motorlarında daha görünür ve daha hızlı olmanızı sağlıyoruz.",
+ 
+    features: [
+      "Daha Performanslı",
+      "Daha Uyumlu",
+      "Daha Tıklamalı",
+    ],
+  },
+];
+
+export const benefits = [
+  {
+    id: "0",
+    title: "Web Tasarım",
+    text: "Tüm tarayıcılar ve mobil cihazlar ile uyumlu, sade ve kolay erişilebilir yapısıyla yeni trend web tasarım hizmetleri vermekteyiz.",
+    backgroundUrl: "./src/assets/benefits/card-1.svg",
+    iconUrl: benefitIcon1,
+  },
+  {
+    id: "1",
+    title: "SEO Optimizasyonu",
+    text: "Belirlediğiniz anahtar kelimelerle, arama motorlarının doğal (reklamsız) aramalarında üst sıralarda siz de yerinizi alın.",
+    backgroundUrl: "./src/assets/benefits/card-2.svg",
+    iconUrl: benefitIcon2,
+    light: true,
+  },
+  {
+    id: "2",
+    title: "Yazılım Geliştirme",
+    text: "Profesyonel ekibimiz tarafından hazırlanmış size özel yazılım çözümleri; iş süreçlerinizi hızlandırır , daha kolay ve verimli yönetmenizi sağlar.",
+    backgroundUrl: "./src/assets/benefits/card-3.svg",
+    iconUrl: benefitIcon3,
+
+  },
+  {
+    id: "3",
+    title: "Danışmanlık",
+    text: "Firmanızın ihtiyaçları analiz edilerek, ihtiyaçlarınızı karşılayacak en uygun program modeli tespit edilip gerekli implementasyon yapılarak çalışmalarımız firmanıza teslim edilir.",
+    backgroundUrl: "./src/assets/benefits/card-4.svg",
+    iconUrl: benefitIcon4,
+
+    light: true,
+  },
+  {
+    id: "4",
+    title: "UI/UX Tasarım",
+    text: "Kullanıcı deneyimi (UX) ve kullanıcı arayüzü (UI) tasarımında uzman ekibimizle, kullanıcı dostu ve estetik arayüzler tasarlayarak, ziyaretçilerinizi memnun eden ve onları müşteriye dönüştüren deneyimler sunuyoruz.",
+    backgroundUrl: "./src/assets/benefits/card-5.svg",
+    iconUrl: benefitIcon1,
+
+  },
+  {
+    id: "5",
+    title: "Dijital Pazarlama",
+    text: "TT Yazılım olarak sizi dijital de üst sıralara çıkaracak ve çevrimiçi itibarınızı arttıracak dijital pazarlama çözümleri sunuyoruz.",
+    backgroundUrl: "./src/assets/benefits/card-6.svg",
+    iconUrl: benefitIcon2,
+
+  },
+];
+
+export const socials = [
+
+  {
+    id: "2",
+    title: "Instagram",
+    iconUrl: instagram,
+    url: "https://instagram.com/tt_yazilim",
+  },
+
+];
