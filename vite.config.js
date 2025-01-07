56e632eeaa5c428be948509eef47b281e4aa09a3
Tunahan Çakıl <tunahancakil@Tunahans-MBP.home>
2025-01-08 01:09:19 +0300

    fix port

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import compression from "vite-plugin-compression";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
@@ -11,6 +10,16 @@ export default defineConfig({
       ext: ".gz",
     }),
   ],
+  server: {
+    open: true,
+    cors: true,
+    compression: true,
+    host: true,
+    port: 8004,
+     watch: {
+       usePolling: true
+     }
+  },
   build: {
     rollupOptions: {
       output: {
@@ -20,6 +29,7 @@ export default defineConfig({
         },
       },
     },
+
     minify: "terser",
     terserOptions: {
       compress: {
